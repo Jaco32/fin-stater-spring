c6bb2f071492350fdebe4085191864ee659cac6b
Jaco32 <jacek.wegorkiewicz@gmail.com>
2025-02-28 15:35:25 +0100

    clean code and design for pko bp transactions manager

--- src/main/java/edu/jaco/fin_stater/transaction/impl/PkoBpTranzMgr.java
+++ src/main/java/edu/jaco/fin_stater/transaction/impl/PkoBpTranzMgr.java
@@ -12,75 +12,31 @@ import org.springframework.stereotype.Component;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Base64;
 import java.util.LinkedList;
 import java.util.List;
 
 @Component
 public class PkoBpTranzMgr extends TransactionManager {
 
+    private final short TRANSACTION_DATE_COLUMN_NUM = 0;
+    private final short TRANSACTION_TYPE_COLUMN_NUM = 2;
+    private final short TRANSACTION_AMOUNT_COLUMN_NUM = 3;
+    private final short TRANSACTION_BALANCE_COLUMN_NUM = 5;
+    private final short TRANSACTION_SENDER_COLUMN_NUM = 7;
+    private final short TRANSACTION_RECEIVER_COLUMN_NUM = 10;
+    private final short TRANSACTION_DESCRIPTION_COLUMN_NUM = 12;
+    private final short TRANSACTION_ADD_INFO_1_COLUMN_NUM = 13;
+    private final short TRANSACTION_ADD_INFO_2_COLUMN_NUM = 14;
+
     public List<Transaction> loadTransactions(String path) {
-        List<Transaction> transactionRows = new LinkedList<>();
+        List<Transaction> transactionRows;
 
-        try {
+        try
+        {
             Workbook workbook = new XSSFWorkbook(path);
-            Sheet sheet = workbook.getSheetAt(0);
-            for (int i = sheet.getFirstRowNum() + 1; i <= sheet.getLastRowNum(); i++) {
-                Transaction transactionRow = new Transaction();
-                Row row = sheet.getRow(i);
-                for (int j = row.getFirstCellNum(); j <= row.getLastCellNum(); j++) {
-                    if (j == 0) { // Data operacji
-                        try {
-                            String dateCellValue = row.getCell(j).getStringCellValue();
-                            transactionRow.setDate(new SimpleDateFormat("yyyy-MM-dd").parse(dateCellValue));
-                        }
-                        catch (IllegalStateException e) {
-                            transactionRow.setDate(DateUtil.getJavaDate(row.getCell(j).getNumericCellValue()));
-                        }
-                    }
-                    if (j == 2) { // Typ tranzakcji
-                        transactionRow.setType(row.getCell(j).getStringCellValue());
-                    }
-                    if (j == 3) { // Kwota
-                        try {
-                            transactionRow.setAmount(row.getCell(j).getNumericCellValue());
-                        } catch (IllegalStateException ex) {
-                            System.out.println("i = " + i);
-                            throw ex;
-                        }
-                    }
-                    if (j == 5) { // Saldo po tranzakcji
-                        transactionRow.setBalance(row.getCell(j).getNumericCellValue());;
-                    }
-                    if (j == 7) { // Nazwa nadawcy
-                        transactionRow.setSender(row.getCell(j).getStringCellValue());
-                    }
-                    if (j == 10) { // Nazwa odbiorcy
-                        transactionRow.setReceiver(row.getCell(j).getStringCellValue());
-                    }
-                    if (j == 12) { // Opis tranzakcji
-                        transactionRow.setDescription(row.getCell(j).getStringCellValue());
-                    }
-                    if (j == 13) { // kolumna N. dodatkowe info - lokalizacja (kraj + miasto), nr telefonu, opis
-                        if(row.getCell(j) != null)
-                            transactionRow.setAdditional_info(row.getCell(j).getStringCellValue());;
-                    }
-                    if (j == 14) { // kolumna O. dodatkowe info - adres strony internetowej
-                        if(row.getCell(j) != null)
-                            transactionRow.setAdditional_info_2(row.getCell(j).getStringCellValue());
-                    }
-                }
-
-                categorizeRow(transactionRow);
-                subCategorizeRow(transactionRow);
-                setTransactionFrequency(transactionRow);
-                transactionRow.setUsedForCalculation(true);
-                transactionRespository.save(transactionRow);
-                transactionRows.add(transactionRow);
-            }
+            transactionRows = parseTransactions(workbook);
         } catch (IOException | ParseException e) {
             throw new RuntimeException(e);
         }
@@ -89,67 +45,74 @@ public class PkoBpTranzMgr extends TransactionManager {
     }
 
     public List<Transaction> loadTransactionsFromAPI(byte[] fileContent) {
-        List<Transaction> transactionRows = new LinkedList<>();
+        List<Transaction> transactionRows;
 
         try {
             InputStream fileIS = new ByteArrayInputStream(fileContent);
             Workbook workbook = new XSSFWorkbook(fileIS);
-            Sheet sheet = workbook.getSheetAt(0);
-            for (int i = sheet.getFirstRowNum() + 1; i <= sheet.getLastRowNum(); i++) {
-                Transaction transactionRow = new Transaction();
-                Row row = sheet.getRow(i);
-                for (int j = row.getFirstCellNum(); j <= row.getLastCellNum(); j++) {
-                    if (j == 0) { // Data operacji
+            transactionRows = parseTransactions(workbook);
+        } catch (IOException | ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        return transactionRows;
+    }
+
+    private List<Transaction> parseTransactions(Workbook workbook) throws ParseException {
+        List<Transaction> transactionRows = new LinkedList<>();
+        Sheet sheet = workbook.getSheetAt(0);
+        for (int i = sheet.getFirstRowNum() + 1; i <= sheet.getLastRowNum(); i++) {
+            Transaction transactionRow = new Transaction();
+            Row row = sheet.getRow(i);
+            for (int j = row.getFirstCellNum(); j <= row.getLastCellNum(); j++) {
+                switch (j) {
+                    case TRANSACTION_DATE_COLUMN_NUM:
                         try {
                             String dateCellValue = row.getCell(j).getStringCellValue();
                             transactionRow.setDate(new SimpleDateFormat("yyyy-MM-dd").parse(dateCellValue));
-                        }
-                        catch (IllegalStateException e) {
+                        } catch (IllegalStateException e) {
                             transactionRow.setDate(DateUtil.getJavaDate(row.getCell(j).getNumericCellValue()));
                         }
-                    }
-                    if (j == 2) { // Typ tranzakcji
+                        break;
+                    case TRANSACTION_TYPE_COLUMN_NUM:
                         transactionRow.setType(row.getCell(j).getStringCellValue());
-                    }
-                    if (j == 3) { // Kwota
+                        break;
+                    case TRANSACTION_AMOUNT_COLUMN_NUM:
                         try {
                             transactionRow.setAmount(row.getCell(j).getNumericCellValue());
                         } catch (IllegalStateException ex) {
                             System.out.println("i = " + i);
                             throw ex;
                         }
-                    }
-                    if (j == 5) { // Saldo po tranzakcji
-                        transactionRow.setBalance(row.getCell(j).getNumericCellValue());;
-                    }
-                    if (j == 7) { // Nazwa nadawcy
+                        break;
+                    case TRANSACTION_BALANCE_COLUMN_NUM:
+                        transactionRow.setBalance(row.getCell(j).getNumericCellValue());
+                        break;
+                    case TRANSACTION_SENDER_COLUMN_NUM:
                         transactionRow.setSender(row.getCell(j).getStringCellValue());
-                    }
-                    if (j == 10) { // Nazwa odbiorcy
+                        break;
+                    case TRANSACTION_RECEIVER_COLUMN_NUM:
                         transactionRow.setReceiver(row.getCell(j).getStringCellValue());
-                    }
-                    if (j == 12) { // Opis tranzakcji
+                        break;
+                    case TRANSACTION_DESCRIPTION_COLUMN_NUM:
                         transactionRow.setDescription(row.getCell(j).getStringCellValue());
-                    }
-                    if (j == 13) { // kolumna N. dodatkowe info - lokalizacja (kraj + miasto), nr telefonu, opis
-                        if(row.getCell(j) != null)
-                            transactionRow.setAdditional_info(row.getCell(j).getStringCellValue());;
-                    }
-                    if (j == 14) { // kolumna O. dodatkowe info - adres strony internetowej
-                        if(row.getCell(j) != null)
+                        break;
+                    case TRANSACTION_ADD_INFO_1_COLUMN_NUM:
+                        if (row.getCell(j) != null)
+                            transactionRow.setAdditional_info(row.getCell(j).getStringCellValue());
+                        break;
+                    case TRANSACTION_ADD_INFO_2_COLUMN_NUM:
+                        if (row.getCell(j) != null)
                             transactionRow.setAdditional_info_2(row.getCell(j).getStringCellValue());
-                    }
                 }
-
-                categorizeRow(transactionRow);
-                subCategorizeRow(transactionRow);
-                setTransactionFrequency(transactionRow);
-                transactionRow.setUsedForCalculation(true);
-                transactionRespository.save(transactionRow);
-                transactionRows.add(transactionRow);
             }
-        } catch (IOException | ParseException e) {
-            throw new RuntimeException(e);
+
+            categorizeRow(transactionRow);
+            subCategorizeRow(transactionRow);
+            setTransactionFrequency(transactionRow);
+            transactionRow.setUsedForCalculation(true);
+            transactionRespository.save(transactionRow);
+            transactionRows.add(transactionRow);
         }
 
         return transactionRows;
